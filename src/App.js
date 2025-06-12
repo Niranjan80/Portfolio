45f3122c18176f2d743da3c1ee5b412d76b2aee3
Niranjan <jagtapniranjan80@gmail.com>
2025-06-13 00:59:41 +0530

    TopNavBar Almost Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import TopNavBar from './components/TopNavBar.jsx';
-import './App.css';
-
+// In your App.js or parent component
+import { useState } from 'react';
+import TopNavBar from './components/TopNavBar';
 function App() {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkTheme(!isDarkTheme);
+    document.body.className = isDarkTheme ? 'light-theme' : 'dark-theme';
+  };
+
   return (
-   <TopNavBar/>
+    <div>
+      <TopNavBar toggleTheme={toggleTheme} isDarkTheme={isDarkTheme} />
+    </div>
   );
 }
-
 export default App;
--- src/components/TopNavBar.jsx
+++ src/components/TopNavBar.jsx
@@ -1,13 +1,42 @@
+import { useState } from 'react';
 import './TopNavBar.css';
-import NavBar from 'react-bootstrap/NavBar';
+import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
-function TopNavBar() {
+import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import { BsSun, BsMoon } from 'react-icons/bs';
+import { FaFileDownload } from 'react-icons/fa';
+
+function TopNavBar({ toggleTheme, isDarkTheme }) {
   return (
-   <NavBar className="navbar" expand="lg">
-    <Container>
-    <NavBar.Brand className="Logo">NIRANJAN</NavBar.Brand>
-    </Container>
-   </NavBar>
+    <Navbar className="navbar-transparent" expand="lg" variant={isDarkTheme ? 'dark' : 'light'}>
+      <Container>
+        <Navbar.Brand className="logo">NIRANJAN</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mx-auto center-nav">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#work">Work</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+          <Nav className="right-items">
+            <Form.Check
+              type="switch"
+              id="theme-switch"
+              className="me-3 d-flex align-items-center"
+              label={isDarkTheme ? <BsMoon className="ms-2"/> : <BsSun className="ms-2"/>}
+              onChange={toggleTheme}
+              checked={isDarkTheme}
+            />
+            <Nav.Link href="/resume.pdf" className="d-flex align-items-center">
+              Resume <FaFileDownload className="ms-1"/>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
+
 export default TopNavBar;
